e6259cda1aa79697efb657bf87a0cfa457883d06
algazza <fadhilaswadira72564@gmail.com>
2025-09-01 11:37:49 +0700

    ✨ feat: close order

--- src/App.vue
+++ src/App.vue
@@ -33,45 +33,31 @@ const isNewCheckout = useLocalStorage<boolean>("isNew", true);
 <template>
   <iframe name="hidden_iframe" style="display: none"></iframe>
 
-  <header class="flex justify-center pt-4">
-    <img :src="header" alt="header" class="w-60 lg:w-80">
-  </header>
-  
-  <main
-    class="py-8 md:px-8 flex flex-col lg:flex-row gap-8 items-center justify-center"
-  >
-    <div class="lg:max-w-1/2 px-12" v-if="isNewCheckout">
-      <Carousel :opts="{
-        loop: true
-      }">
-        <CarouselContent class="items-center">
-          <CarouselItem
-            v-for="img in imageArray"
-            :key="img"
-            class="flex items-center"
-          >
-            <img :src="img" :alt="img" />
-          </CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
-    </div>
-
-    <div
-      class="p-4 w-full bg-white rounded-lg"
-      :class="!isNewCheckout ? 'max-w-[580px]' : ''"
+  <div :class="isNewCheckout ? 'flex justify-center items-center flex-col h-dvh' : ''">
+    <header class="flex items-center pt-4 flex-col">
+      <img :src="header" alt="header" class="w-60 lg:w-80">
+      <h2 class="text-2xl font-bold mt-4">Pengisian form telah ditutup. <br>Terimakasih sudah memesan:)</h2>
+    </header>
+    
+    <main
+      class="pt-8 md:px-8 flex flex-col lg:flex-row gap-8 items-center justify-center"
     >
-      <FormSuccess :data="formData" v-if="!isNewCheckout" />
-      <FormComp v-else />
-    </div>
-  </main>
-  <footer class="flex justify-center py-8">
-    <p class="text-gray-500 text-sm">
-      &copy; All Rights Reserved By
-      <a href="https://www.instagram.com/osis8_store/" class="font-bold"
-        >@osis8_store.</a
+  
+      <div
+        class="w-full bg-white rounded-lg"
+        :class="!isNewCheckout ? 'max-w-[580px] p-4' : ''"
+        v-if="!isNewCheckout"
       >
-    </p>
-  </footer>
+        <FormSuccess :data="formData"  />
+      </div>
+    </main>
+    <footer class="flex justify-center py-8">
+      <p class="text-gray-500 text-sm">
+        &copy; All Rights Reserved By
+        <a href="https://www.instagram.com/osis8_store/" class="font-bold"
+          >@osis8_store.</a
+        >
+      </p>
+    </footer>
+  </div>
 </template>
